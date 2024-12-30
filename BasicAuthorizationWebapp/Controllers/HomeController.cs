4914f11e3e0a3a14a73be01b323e3da229dbf459
gauravint <gauravraut659@gmail.com>
2024-12-30 19:17:41 +0530

    Add project files.

---
+++ BasicAuthorizationWebapp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using BasicAuthorizationWebapp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BasicAuthorizationWebapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
